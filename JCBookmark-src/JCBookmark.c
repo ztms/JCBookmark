b5e07af7b80064a610122f079ec82bfe82ac9cea
ztms <ztmsdf@gmail.com>
2015-07-18 20:22:24 +0900

    v2.2

--- JCBookmark-src/JCBookmark.c
+++ JCBookmark-src/JCBookmark.c
@@ -128,7 +128,7 @@
 #define		WM_CONFIG_DIALOG	(WM_APP+4)		// 設定ダイアログ後処理
 #define		WM_TABSELECT		(WM_APP+5)		// 設定ダイアログ初期表示タブのためのメッセージ
 #define		WM_THREADFIN		(WM_APP+6)		// スレッド終了メッセージ
-#define		APPNAME				L"JCBookmark v2.2dev"
+#define		APPNAME				L"JCBookmark v2.2"
 #define		MY_INI				L"my.ini"
 
 HWND		MainForm			= NULL;				// メインフォームハンドル
@@ -183,7 +183,7 @@ SSL_CTX*	ssl_ctx				= NULL;				// SSLコンテキスト
 // memory.log がクリアされず既存の memory.log が存在したら追記されていってしまうので注意。
 // 回避が面倒なのでとりあえず手動で memory.log を消す運用で対処。
 //
-#define MEMLOG
+//#define MEMLOG
 #ifdef MEMLOG
 FILE* mlog=NULL;
 void _mlogopen( BOOL delete )
